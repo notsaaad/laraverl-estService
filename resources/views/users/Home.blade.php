d496f6b7531e039d82b9b6446b8185fe4b5a04ee
AlaaHany2003 <hanyalaahany2003@gmail.com>
2025-06-23 11:44:23 +0300

    Home

--- resources/views/users/Home.blade.php
+++ resources/views/users/Home.blade.php
@@ -145,13 +145,13 @@
           <img src="{{ URL::asset('public/images/users/Home/1.png') }}" alt="">
         </div>
         <div class="col-sm-12 col-md-6 col-lg-3">
-          <img src="{{ URL::asset('public/images/users/Home/1.png') }}" alt="">
+          <img src="{{ URL::asset('public/images/users/Home/2.png') }}" alt="">
         </div>
         <div class="col-sm-12 col-md-6 col-lg-3">
-          <img src="{{ URL::asset('public/images/users/Home/1.png') }}" alt="">
+          <img src="{{ URL::asset('public/images/users/Home/3.png') }}" alt="">
         </div>
         <div class="col-sm-12 col-md-6 col-lg-3">
-          <img src="{{ URL::asset('public/images/users/Home/1.png') }}" alt="">
+          <img src="{{ URL::asset('public/images/users/Home/4.png') }}" alt="">
         </div>
       </div>
     </section>
